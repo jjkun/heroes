abe24ad6e449a71d0ad7800d9e4711bc3e0247d2
jperea <jperea@mailprocp.com>
2019-01-10 19:36:14 -0600

    First commit

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../servicios/heroes.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styles: []
+})
+export class HeroesComponent implements OnInit {
+
+  heroes:Heroe[] = [];
+
+  constructor( private _heroesService:HeroesService, private _router:Router ) { 
+  }
+
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+  }
+
+  verHeroe(idx:number){
+    this._router.navigate(['/heroe',idx]);
+  }
+
+
+}
